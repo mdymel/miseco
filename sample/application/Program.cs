b79adae304b25df66a689e41f76ec155b02727f2
Michal Dymel <michal@dymel.pl>
2016-03-29 10:10:11 +0200

    Proxy generation using Castle.DynamicProxy library

--- sample/application/Program.cs
+++ sample/application/Program.cs
@@ -1,15 +1,23 @@
-﻿namespace MiSeCo.Sample.Application
+﻿using System;
+
+namespace MiSeCo.Sample.Application
 {
     public class Program
     {
         public interface IFirstDynamicInterface : IContractInterface
         {
+            int Add(int a, int b);
+            string GetString();
         }
 
         public static void Main(string[] args)
         {
             var miseco = new MiSeCo();
             var service1 = miseco.CreateServiceObject<IFirstDynamicInterface>();
+            int c = service1.Add(2, 2);
+            Console.WriteLine(c);
+            string s = service1.GetString();
+            Console.WriteLine($"s is null: {s == null}");
         }
     }
 }
--- src/miseco/MiSeCo.cs
+++ src/miseco/MiSeCo.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using Castle.Core.Interceptor;
+using Castle.DynamicProxy;
 
 namespace MiSeCo
 {
+    internal class MethodInterceptor : IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+            Console.WriteLine($"New call: {invocation.Method.Name}");
+            Type returnType = invocation.Method.ReturnType;
+            invocation.ReturnValue = returnType.IsValueType
+                ? Activator.CreateInstance(returnType)
+                : null;
+        }
+    }
+
     public class MiSeCo
     {
-        public T CreateServiceObject<T>() where T:IContractInterface
-        {
-            throw new NotImplementedException();
-        }
+    public T CreateServiceObject<T>() where T:IContractInterface
+    {
+        var generator = new ProxyGenerator();
+        var proxy = generator.CreateInterfaceProxyWithoutTarget(typeof(T), new MethodInterceptor());
+        return (T)proxy;
+    }
     }
 }
